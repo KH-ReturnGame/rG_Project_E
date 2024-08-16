8c7b6ac58ab4b3930eadefac49bbd526f907e6a2
이형주 <jujumom630@gmail.com>
2024-08-17 02:23:28 +0900

    맵 탈출 수정
    그냥 굳이ground가져오는 거 대신에 그냥 wall ground따로 만들어서ground는 istrigger로 해놓고 wall은 진짜 콜라이더로 하면 탈출 안할거 같아서 이렇게 수정함

--- Assets/#1 Scripts/#1 Entity/Player/Player_Movement.cs
+++ Assets/#1 Scripts/#1 Entity/Player/Player_Movement.cs
@@ -24,14 +24,13 @@ public class Player_Movement : MonoBehaviour
         _playerRigidbody = GetComponent<Rigidbody2D>();
         _player = GetComponent<Player>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        _player.AddState(PlayerStates.CanDash);
         _playerCollider = GetComponent<Collider2D>();
+        _player.AddState(PlayerStates.CanDash);
     }
 
     //매 프레임 실행
     void Update()
     {
-        skilluse();
         x = _playerRigidbody.position.x;
         y = _playerRigidbody.position.y;
     }
@@ -41,38 +40,15 @@ public class Player_Movement : MonoBehaviour
     {
         movement();
     }
+
     void movement()
     {
         inputVec.x = Input.GetAxisRaw("Horizontal");
         inputVec.y = Input.GetAxisRaw("Vertical");
-        Vector2 HorizontalVec = new Vector2(_playerRigidbody.position.x + inputVec.x, _playerRigidbody.position.y);
-        Debug.DrawRay(HorizontalVec, Vector2.down, new Color(0, 1, 0));
-        RaycastHit2D HrayHit = Physics2D.Raycast(HorizontalVec, Vector2.down, 1, LayerMask.GetMask("platform"));
-        Vector2 VerticalVec = new Vector2(_playerRigidbody.position.x, _playerRigidbody.position.y + inputVec.y);
-        Debug.DrawRay(VerticalVec, Vector2.left, new Color(0, 1, 0));
-        RaycastHit2D VrayHit = Physics2D.Raycast(VerticalVec, Vector2.left, 1, LayerMask.GetMask("platform"));
-        if (HrayHit.collider == null)
-        {
-            inputVec.x = 0;
-        }
-        if (VrayHit.collider == null)
-        {
-            inputVec.y = 0;
-        }
+        
         Vector2 nextvec = inputVec.normalized * speed * Time.fixedDeltaTime;
         _playerRigidbody.MovePosition(_playerRigidbody.position + nextvec);
         Debug.Log(inputVec.x);
-    }
-    void skilluse()
-    {
-        if (Input.GetKeyDown("y"))
-        {
-            knuck = true;
-        }
-        else
-        {
-            knuck = false;
-        }
     }
 }
 
