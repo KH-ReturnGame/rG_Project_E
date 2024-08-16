1de017db925fc3fc38c68bb8e6c51b46b5d84def
이형주 <jujumom630@gmail.com>
2024-08-17 02:27:48 +0900

    발차기 시스템을 바꿀 대대적인 준비 완료
    음참마속

--- Assets/#1 Scripts/#1 Entity/Player/skill.cs
+++ Assets/#1 Scripts/#1 Entity/Player/skill.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class skill : MonoBehaviour
 {
-    public Vector2 vec = new Vector2(0, 2);
-    private GameObject player;
-
     void Start()
     {
-        player = transform.parent.gameObject;
+        
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Q))
         {
-            transform.localScale = new Vector2(2, 2);
-        }
-        else
-        {
-            transform.localScale = new Vector2(0.1f, 0.1f);
+            Debug.Log("발차기 독도킥");
         }
     }
-    void FixedUpdate()
-    {
-        transform.position = player.GetComponent<Transform>().position;
-    }
-
 }
