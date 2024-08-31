1e24a9afbb5b5de878721a54311fada6f7db0019
mugglewoojin <mugglewoojin@gmail.com>
2024-08-31 13:16:28 +0900

    저장

--- Assets/#1 Scripts/EnemyMove.cs
+++ Assets/#1 Scripts/EnemyMove.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    public GameObject player;
-    float angle;
+    //public GameObject player;
+    //float angle;
+    public float speed;
+    public Rigidbody2D target;
+    Rigidbody2D rigid;
 
-    void Update()
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    /*void Update()
     {
         angle = Mathf.Atan2(player.transform.position.y - transform.position.y,
                             player.transform.position.x - transform.position.x)
               * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle - 90);
-    }
+    }*/
 
     void FixedUpdated()
     {
-        Vector2 vec = new Vector2(0, 0);
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nexcVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nexcVec);
+        rigid.velocity = Vector2.zero;
     }
 }
