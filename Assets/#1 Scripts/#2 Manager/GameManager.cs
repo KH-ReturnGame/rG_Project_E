e7a98976d5d172bebce76e89477910ec15d3b458
이형주 <jujumom630@gmail.com>
2024-09-02 13:22:37 +0900

    폴더 정리 및 ESC메뉴 구현

---
+++ Assets/#1 Scripts/#2 Manager/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject _EscMenu;
+    public bool IsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsMenu = false;// 나중에 없어도 되...려나?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            IsMenu = !IsMenu;
+            if(IsMenu)
+            {
+                _EscMenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                _EscMenu.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
