058dc706022edd90d6231a0a10364aec5c5d7c95
이형주 <jujumom630@gmail.com>
2024-09-15 08:40:31 +0900

    화살 완성ㅎ

--- Assets/#1 Scripts/#1 Entity/Trap/ArrowLauncher.cs
+++ Assets/#1 Scripts/#1 Entity/Trap/ArrowLauncher.cs
@@ -6,6 +6,7 @@ public class ArrowLauncher : MonoBehaviour
 {
     public GameObject Arrow;
     public float cooldown;
+    public int type; // 1 : 동, 2 : 서, 3 : 남, 4 : 북
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +21,30 @@ public class ArrowLauncher : MonoBehaviour
 
     IEnumerator LaunchArrow()
     {
-        GameObject.Instantiate(GameObject Arrow, Vector3 transform.position, Quternian rotation);
-        return null;
+        GameObject ArrowObj = Object.Instantiate(Arrow, transform.position, Quaternion.identity);
+        Arrow _arrow = ArrowObj.GetComponent<Arrow>();
+        switch (type)
+        {
+            case 1: // 동 (East)
+                ArrowObj.transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            case 2: // 서 (West)
+                ArrowObj.transform.rotation = Quaternion.Euler(0, 180, 0);
+                break;
+            case 3: // 남 (South)
+                ArrowObj.transform.rotation = Quaternion.Euler(0, 0, -90);
+                break;
+            case 4: // 북 (North)
+                ArrowObj.transform.rotation = Quaternion.Euler(0, 0, 90);
+                break;
+            default:
+                Debug.Log("Invalid direction type");
+                break;
+        }
+        _arrow.ChangeType(type);
+
+        yield return new WaitForSeconds(cooldown); // 쿨다운
+
+        StartCoroutine(LaunchArrow());
     }
 }
