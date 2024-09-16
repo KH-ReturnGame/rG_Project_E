c247cd3aef8f53bb0fe9a195bcfcc946378d18af
이형주 <jujumom630@gmail.com>
2024-09-17 03:53:09 +0900

    무궁화 함정 기본 스크립트

---
+++ Assets/#1 Scripts/#1 Entity/Trap/TrapSharon.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSharon : MonoBehaviour
+{
+    bool IsPowerOn;
+    public float pivotX;
+    public float pivotY;
+    GameObject DetectRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsPowerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
