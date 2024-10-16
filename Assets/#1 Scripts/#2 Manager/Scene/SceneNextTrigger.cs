7a2a9eec1354afaad8f638e8a4ac1dbf6c076bd7
이형주 <jujumom630@gmail.com>
2024-10-16 17:48:58 +0900

    다음 씬으로 넘어가는거
    B팀꺼 파쿠리(본인이 본인을 파쿠리하는 카카시의 경지)

---
+++ Assets/#1 Scripts/#2 Manager/Scene/SceneNextTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneNextTrigger : MonoBehaviour
+{
+    public string NextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneLoadManager.Instance.LoadScene(NextScene);
+        }
+    }
+}
