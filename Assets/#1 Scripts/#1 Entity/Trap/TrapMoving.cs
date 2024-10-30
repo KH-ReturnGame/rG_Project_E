be8970f0d404027d30188310dcfd03c795d11e17
이형주 <jujumom630@gmail.com>
2024-10-30 19:36:58 +0900

    맵 수정
    그 시발 뭐시냐 움직이는 함정 피벗이랑 관련 psd수정
    + 움직이는거 속도 랜덤

--- Assets/#1 Scripts/#1 Entity/Trap/TrapMoving.cs
+++ Assets/#1 Scripts/#1 Entity/Trap/TrapMoving.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TrapMoving : MonoBehaviour
 {
     public int direction;
+    public float speed;
     public GameObject pivot1;
     public GameObject pivot2;
 
@@ -12,6 +13,7 @@ public class TrapMoving : MonoBehaviour
     void Start()
     {
         direction = 1;
+        speed = Random.Range(1f, 5f);
     }
 
     // Update is called once per frame
@@ -24,11 +26,11 @@ public class TrapMoving : MonoBehaviour
     {
         if(direction == 1)
         {
-            transform.position = Vector3.MoveTowards(transform.position, pivot1.transform.position, Time.deltaTime * 2);
+            transform.position = Vector3.MoveTowards(transform.position, pivot1.transform.position, Time.deltaTime * 2 * speed);
         }
         else if(direction == 2)
         {
-            transform.position = Vector3.MoveTowards(transform.position, pivot2.transform.position, Time.deltaTime * 2);
+            transform.position = Vector3.MoveTowards(transform.position, pivot2.transform.position, Time.deltaTime * 2 * speed);
         }
     }
 
