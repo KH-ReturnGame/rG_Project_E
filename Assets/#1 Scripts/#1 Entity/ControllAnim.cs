9aa2d3dcf4a85cf378141ed0bad9eed0cc535b3f
이형주 <jujumom630@gmail.com>
2024-12-19 03:16:50 +0900

    사분면으로 애니메이터 enabled조정하기

--- Assets/#1 Scripts/#1 Entity/ControllAnim.cs
+++ Assets/#1 Scripts/#1 Entity/ControllAnim.cs
@@ -18,15 +18,77 @@ public class ControllAnim : MonoBehaviour
         FrontAnim = FrontObj.GetComponent<Animator>();
         BackAnim = BackObj.GetComponent<Animator>();
         SideAnim = SideObj.GetComponent<Animator>();
-        //playerPos = 뭐시기;
+        //playerPos = 뭐시기 뭐 우진이가 짠 무브에서 가져와야징;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerPos.position.x > 10)
+        float deltaX = Mathf.Abs(playerPos.position.x - transform.position.x);
+        float deltaY = Mathf.Abs(playerPos.position.y - transform.position.y);
+        if(playerPos.position.x > transform.position.x && playerPos.position.y > transform.position.y)
         {
-            
+            if(deltaX > deltaY)
+            {
+                FrontAnim.enabled = false;
+                BackAnim.enabled = false;
+                SideAnim.enabled = true;
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                FrontAnim.enabled = false;
+                BackAnim.enabled = true;
+                SideAnim.enabled = false;
+            }
+        }
+        else if(playerPos.position.x > transform.position.x && playerPos.position.y < transform.position.y)
+        {
+            if(deltaX > deltaY)
+            {
+                FrontAnim.enabled = false;
+                BackAnim.enabled = false;
+                SideAnim.enabled = true;
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                FrontAnim.enabled = true;
+                BackAnim.enabled = false;
+                SideAnim.enabled = false;
+            }
+        }
+        else if(playerPos.position.x < transform.position.x && playerPos.position.y > transform.position.y)
+        {
+            if(deltaX > deltaY)
+            {
+                FrontAnim.enabled = false;
+                BackAnim.enabled = false;
+                SideAnim.enabled = true;
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                FrontAnim.enabled = false;
+                BackAnim.enabled = true;
+                SideAnim.enabled = false;
+            }
+        }
+        else if(playerPos.position.x < transform.position.x && playerPos.position.y < transform.position.y)
+        {
+            if(deltaX > deltaY)
+            {
+                FrontAnim.enabled = false;
+                BackAnim.enabled = false;
+                SideAnim.enabled = true;
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                FrontAnim.enabled = true;
+                BackAnim.enabled = false;
+                SideAnim.enabled = false;
+            }
         }
     }
 }
