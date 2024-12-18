0f8c451110f472f1bf724ee2f12552a2f6a898f3
이형주 <jujumom630@gmail.com>
2024-12-19 02:56:05 +0900

    프리팹에 애니메이터 재연결

--- Assets/#1 Scripts/#1 Entity/ControllAnim.cs
+++ Assets/#1 Scripts/#1 Entity/ControllAnim.cs
@@ -11,17 +11,22 @@ public class ControllAnim : MonoBehaviour
     Animator FrontAnim;
     Animator BackAnim;
     Animator SideAnim;
+    Transform playerPos;
     // Start is called before the first frame update
     void Start()
     {
         FrontAnim = FrontObj.GetComponent<Animator>();
         BackAnim = BackObj.GetComponent<Animator>();
         SideAnim = SideObj.GetComponent<Animator>();
+        //playerPos = 뭐시기;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerPos.position.x > 10)
+        {
+            
+        }
     }
 }
