5297979a37032868e07edcae6260070629f2f613
이형주 <jujumom630@gmail.com>
2024-12-19 02:04:00 +0900

    anim컨트롤러 생성 및 연결

---
+++ Assets/#1 Scripts/#1 Entity/ControllAnim.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllAnim : MonoBehaviour
+{
+    public GameObject FrontObj;
+    public GameObject BackObj;
+    public GameObject SideObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
